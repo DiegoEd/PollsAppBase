86d472a9b3499e413978c3ff01abd0eb123e3c32
Diego Aguilera <diegoaguilerac@hotmail.com>
2017-12-23 10:09:36 -0400

    Poll Model

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171222212712) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "polls", force: :cascade do |t|
     t.string "title"
     t.string "description"
